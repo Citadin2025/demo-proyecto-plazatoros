a295d83916f94d3f31ba7a2cc80e07cafe1a4589
Nicolás Carro <nicocarro.07@gmail.com>
2025-11-06 17:11:46 -0300

    console errors removal

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,14 @@
  * Change Navbar color while scrolling
 */
 
-$(window).scroll(function(){
-	handleTopNavAnimation();
+$(window).scroll(function () {
+    handleTopNavAnimation();
 });
 
-$(window).load(async function(){
-	handleTopNavAnimation();
+$(window).load(async function () {
+    handleTopNavAnimation();
 
+    /*
     const els = {
         'fecha': document.getElementById('intro-fecha'),
         'titulo': document.getElementById('intro-titulo'),
@@ -22,63 +23,62 @@ $(window).load(async function(){
         }
     );
     const res = await data.json();
-    
+
     els.fecha.innerHTML = res.response.fecha;
     els.titulo.innerHTML = res.response.nombre;
     els.descripcion.innerHTML = res.response.descripcion;
-    els.link.href = res.response.linkDeCompra;
+    els.link.href = res.response.linkDeCompra; */
 });
 
 function handleTopNavAnimation() {
-	var top=$(window).scrollTop();
+    var top = $(window).scrollTop();
 
-	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
-	}
-	else{
-		$('#site-nav').removeClass('navbar-solid'); 
-	}
+    if (top > 10) {
+        $('#site-nav').addClass('navbar-solid');
+    }
+    else {
+        $('#site-nav').removeClass('navbar-solid');
+    }
 }
 
 /*
  * Registration Form
 */
 
-$('#registration-form').submit(function(e){
+$('#registration-form').submit(function (e) {
     e.preventDefault();
-    
+
     var postForm = { //Fetch form data
-            'fname'     : $('#registration-form #fname').val(),
-            'lname'     : $('#registration-form #lname').val(),
-            'email'     : $('#registration-form #email').val(),
-            'cell'      : $('#registration-form #cell').val(),
-            'address'   : $('#registration-form #address').val(),
-            'zip'       : $('#registration-form #zip').val(),
-            'city'      : $('#registration-form #city').val(),
-            'program'   : $('#registration-form #program').val()
+        'fname': $('#registration-form #fname').val(),
+        'lname': $('#registration-form #lname').val(),
+        'email': $('#registration-form #email').val(),
+        'cell': $('#registration-form #cell').val(),
+        'address': $('#registration-form #address').val(),
+        'zip': $('#registration-form #zip').val(),
+        'city': $('#registration-form #city').val(),
+        'program': $('#registration-form #program').val()
     };
 
     $.ajax({
-            type      : 'POST',
-            url       : './assets/php/contact.php',
-            data      : postForm,
-            dataType  : 'json',
-            success   : function(data) {
-                            if (data.success) {
-                                $('#registration-msg .alert').html("Registration Successful");
-                                $('#registration-msg .alert').removeClass("alert-danger");
-                                $('#registration-msg .alert').addClass("alert-success");
-                                $('#registration-msg').show();
-                            }
-                            else
-                            {
-                                $('#registration-msg .alert').html("Registration Failed");
-                                $('#registration-msg .alert').removeClass("alert-success");
-                                $('#registration-msg .alert').addClass("alert-danger");
-                                $('#registration-msg').show();
-                            }
-                        }
-        });
+        type: 'POST',
+        url: './assets/php/contact.php',
+        data: postForm,
+        dataType: 'json',
+        success: function (data) {
+            if (data.success) {
+                $('#registration-msg .alert').html("Registration Successful");
+                $('#registration-msg .alert').removeClass("alert-danger");
+                $('#registration-msg .alert').addClass("alert-success");
+                $('#registration-msg').show();
+            }
+            else {
+                $('#registration-msg .alert').html("Registration Failed");
+                $('#registration-msg .alert').removeClass("alert-success");
+                $('#registration-msg .alert').addClass("alert-danger");
+                $('#registration-msg').show();
+            }
+        }
+    });
 });
 
 /*
@@ -88,7 +88,7 @@ $('#registration-form').submit(function(e){
 smoothScroll.init();
 
 // Defensive: ensure FAQ accordion toggles correctly even if scripts load in odd order
-;(function ($) {
+; (function ($) {
     $(function () {
         // Only bind once
         if ($.fn.collapse && !window.__faqAccordionInitialized) {
